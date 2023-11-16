f578365ab8610114a76682b4d81790ee1033d8b1
Roger Shagawat <34222430+rogerShagawat@users.noreply.github.com>
2023-11-16 13:36:07 -0500

    added poemRoutes to routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,17 @@
 // Amalgamate routes here!
-<<<<<<< HEAD
-import routes from './routing.js'
-import auth from './auth.js'
+import routes from "./routing.js";
+import poemRoutes from "./poems.js";
 
 const constructorMethod = (app) => {
-  app.use('/', routes);
-  app.use('/', auth);
+   app.use("/", routes);
+   app.use("/poems", poemRoutes);
 
-  app.use('*', (req, res) => {
-    res.status(404).render("error",{
-      title: "Error", error:" 404 Not Found"
-    });
-  });
+   app.use("*", (req, res) => {
+      res.status(404).render("error", {
+         title: "Error",
+         error: " 404 Not Found",
+      });
+   });
 };
-  
+
 export default constructorMethod;
-=======
-import poemRoutes from "./poems.js"
-
-const constructorMethod = (app) => {
-   app.use('/poems',  poemRoutes);
-};
->>>>>>> 73d3344 (preliminary data functions and routing for poems)
