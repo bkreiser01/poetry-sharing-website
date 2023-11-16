d2f90b44f91a47a29485866f47b3f6485842ecf1
Roger Shagawat <34222430+rogerShagawat@users.noreply.github.com>
2023-11-16 13:33:16 -0500

    added delete route to poems

--- routes/poems.js
+++ routes/poems.js
@@ -6,17 +6,21 @@ import { poemData } from "../data/index.js";
 
 router.route("/new").get(async (req, res) => {
    const users = await userData.getAllUsers();
-   res.render("poems/new", { users: users });
+   res.render("newpoem", { users: users });
 });
+
+// For this we probably want to use middle ware
+// to check if the user is who they say they are
+// (for privated poems and posting/updating poems)
 router
    .route("/:id")
    .get(async (req, res) => {
       try {
-         // Validate
-         req.params.id = req.params.id;
+         // TODO Validate id
+         req.params.id = req.params.id; // Validation standin replace
       } catch (e) {
          // Make this an error html page
-         return res.status(400).json({ error: e });
+         return res.status(400).render("error", { error: e });
       }
       try {
          const poem = await poemData.getPoemById(req.params.id);
@@ -25,4 +29,18 @@ router
       }
    })
    .put(async (req, res) => {})
-   .patch(async (req, res) => {});
+   .patch(async (req, res) => {})
+   .delete(async (req, res) => {
+      // TODO Validate id
+
+      try {
+         let deletedPoem = await poemData.removePoem(req.params.id);
+
+         // TODO need poem delete handlebar
+         res.status(200).json(deletedPoem);
+      } catch (e) {
+         let status = e[0];
+         let message = e[1];
+         res.status(status).json({ error: message });
+      }
+   });
