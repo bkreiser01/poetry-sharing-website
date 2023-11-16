357d292fbd352ce108c706b992a145ba7a44bec6
Brandon Kreiser <bkreiser5@gmail.com>
2023-11-16 12:53:09 -0500

    template code to get the site to run to get ready to add routes

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,8 @@
 // Amalgamate routes here!
+const constructorMethod = (app) => {
+    app.use('*', (req, res) => {
+      res.status(404).render("error",{title: "Error", error:" 404 Not Found"});
+    });
+  };
+  
+  export default constructorMethod;
