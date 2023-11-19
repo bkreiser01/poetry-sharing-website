5f1ea306c25e3c338bffd89d9c07b304521b1402
Adrien Huet <ahuet@stevens.com>
2023-11-19 13:47:45 -0500

    test(app): added basic file architecture for testing and code coverage with jest. Run tests with "npm run test".

--- test.js
+++
@@ -1,1 +0,0 @@
-// Do testing from this file
---
+++ tests/app.test.js
@@ -0,0 +1,5 @@
+// Define a testsuite like this
+describe("testfiles", () => {
+  // And a test like this
+  test("should have a passing test", () => {});
+});
