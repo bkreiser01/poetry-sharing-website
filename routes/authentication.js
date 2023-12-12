adc4364175e1b84887af58f03b44b32140309885
Adrien Huet <ahuet@stevens.edu>
2023-12-12 17:02:35 -0500

    feat(handlebars): added a layout with no session-dependent elements for /login and /register

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,57 +1,56 @@
 // Package Imports
-import {Router} from 'express';
+import { Router } from "express";
 const router = Router();
 
 // Local Imports
-import validation from '../helpers/validation.js'
-import user from '../data/user.js'
+import validation from "../helpers/validation.js";
+import user from "../data/user.js";
 
 // Login route
-router.route('/login')
+router
+  .route("/login")
 
-    // GET /login
-    .get(async (req, res) => {
-        res.status(200).render('login', {title:"Login"});
-    })
+  // GET /login
+  .get(async (req, res) => {
+    res.status(200).render("login", { layout: "no_session", title: "Login" });
+  })
 
-    // POST /login
-    .post(async (req, res) => {
-        try {
-            // Get the login user data from the request body
-            let loginUser = req.body;
+  // POST /login
+  .post(async (req, res) => {
+    try {
+      // Get the login user data from the request body
+      let loginUser = req.body;
 
-            // Validate the login user
-            loginUser = validation.checkLoginFields(loginUser);
+      // Validate the login user
+      loginUser = validation.checkLoginFields(loginUser);
 
-            // Attempt to login the user
-            loginUser = await user.login(
-                loginUser.username,
-                loginUser.password
-            );
-            
-            // Delete the hashed password from the user object for security
-            delete loginUser.hashedPassword
+      // Attempt to login the user
+      loginUser = await user.login(loginUser.username, loginUser.password);
 
-            // Set the session user
-            req.session.user = loginUser;
-        
-            throw new Error("Not implemented");
-        } catch (e) {
-            return res.status(500).json({error: e.message});
-        }
-    })
+      // Delete the hashed password from the user object for security
+      delete loginUser.hashedPassword;
+
+      // Set the session user
+      req.session.user = loginUser;
+
+      throw new Error("Not implemented");
+    } catch (e) {
+      return res.status(500).json({ error: e.message });
+    }
+  });
 
 // Register route
-router.route('/register')
+router
+  .route("/register")
 
-    // GET /register
-    .get(async (req, res) => {
-        res.status(200).render('register', {title:"Register"});
-    })
+  // GET /register
+  .get(async (req, res) => {
+    res
+      .status(200)
+      .render("register", { layout: "no_session", title: "Register" });
+  })
 
-    // POST /register
-    .post(async (req, res) => {
-
-    })
+  // POST /register
+  .post(async (req, res) => {});
 
 export default router;
