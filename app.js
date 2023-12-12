521d6c831f6861a219e208136682c095cdb81c21
Adrien Huet <ahuet@stevens.edu>
2023-12-12 16:56:15 -0500

    feat(handlebars): app.js now uses default.handlebars as default layout

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-import express from 'express';
+import express from "express";
 const app = express();
-import session from 'express-session';
-import configRoutes from './routes/index.js';
-import {fileURLToPath} from 'url';
-import {dirname} from 'path';
-import exphbs from 'express-handlebars';
+import session from "express-session";
+import configRoutes from "./routes/index.js";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import exphbs from "express-handlebars";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const staticDir = express.static(__dirname + '/public');
+const staticDir = express.static(__dirname + "/public");
 
 const rewriteUnsupportedBrowserMethods = (req, res, next) => {
   // If the user posts to the server with a property called _method, rewrite the request's method
@@ -23,27 +23,26 @@ const rewriteUnsupportedBrowserMethods = (req, res, next) => {
   next();
 };
 
-app.use('/public', staticDir);
+app.use("/public", staticDir);
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(rewriteUnsupportedBrowserMethods);
 
-app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+app.engine("handlebars", exphbs.engine({ defaultLayout: "default" }));
+app.set("view engine", "handlebars");
 
 app.use(
   session({
-    name: 'AuthState',
+    name: "AuthState",
     secret: "M6Mn#APSxDX#MNaWqSD#StafG",
     saveUninitialized: false,
     resave: false,
-    cookie: {maxAge: 60000}
+    cookie: { maxAge: 60000 },
   })
 );
 
 configRoutes(app);
-
-app.listen(80, () => {
+app.listen(3000, () => {
   console.log("We've now got a server!");
-  console.log('Your routes will be running on http://localhost');
+  console.log("Your routes will be running on http://localhost");
 });
