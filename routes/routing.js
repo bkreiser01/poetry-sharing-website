04cbdad8e43773b180682f4babd0de0ad3a5ace5
Adrien Huet <ahuet@stevens.edu>
2023-12-12 16:58:51 -0500

    refactor(coding-style): fixed style for routing.js

--- routes/routing.js
+++ routes/routing.js
@@ -1,59 +1,58 @@
 //import express and express router as shown in lecture code and worked in previous labs.  Import your data functions from /data/characters.js that you will call in your routes below
-import {Router} from 'express';
-import {CreateNewTag, SearchForTag} from '../data/tags.js';
+import { Router } from "express";
+import { CreateNewTag, SearchForTag } from "../data/tags.js";
 //import {seedDb} from '../config/seed.js';
 const router = Router();
 
-router.route('/').get(async (req, res) => {
-    //code here for GET will render the home handlebars file
-    res.render('homepage', {title:"Poetry Sharing"});
+router.route("/").get(async (req, res) => {
+  //code here for GET will render the home handlebars file
+  res.render("homepage", { title: "Poetry Sharing" });
 });
 
-router.route('/user').get(async (req, res) => {
-    res.render('user', {title:"User"});
+router.route("/user").get(async (req, res) => {
+  res.render("user", { title: "User" });
 });
 
-
-router.route('/poem').get(async (req, res) => {
-    res.render('poem', {title:"Poem"});
+router.route("/poem").get(async (req, res) => {
+  res.render("poem", { title: "Poem" });
 });
 
-router.route('/editor').get(async (req, res) => {
-    res.render('editor', {title:"Editor"});
+router.route("/editor").get(async (req, res) => {
+  res.render("editor", { title: "Editor" });
 });
 
-router.route('/tagTest').get(async (req, res) => {
-    res.render('tagTest', {title:"Testing Tags"});
+router.route("/tagTest").get(async (req, res) => {
+  res.render("tagTest", { title: "Testing Tags" });
 });
 
-router.route('/CreateNewTag').post(async (req, res) => {
-    //Route to add new tag to the database
-    //Validate here
+router.route("/CreateNewTag").post(async (req, res) => {
+  //Route to add new tag to the database
+  //Validate here
 
-    //Add to database
-    let TagString = req.body.newTagString;
-    let PoemID = req.body.newTaggedPoem;
-    await CreateNewTag(TagString, PoemID);
+  //Add to database
+  let TagString = req.body.newTagString;
+  let PoemID = req.body.newTaggedPoem;
+  await CreateNewTag(TagString, PoemID);
 
-    //Refresh page
-    return res.redirect('/tagTest');
+  //Refresh page
+  return res.redirect("/tagTest");
 });
 
-router.route('/SearchForTag').post(async (req, res) => {
-    //Route to search for tags in the database
-    //Validate here
+router.route("/SearchForTag").post(async (req, res) => {
+  //Route to search for tags in the database
+  //Validate here
 
-    //Search for tag
-    let TagString = req.body.SearchTagString;
-    let FoundTag = await SearchForTag(TagString);
-    console.log(FoundTag);
+  //Search for tag
+  let TagString = req.body.SearchTagString;
+  let FoundTag = await SearchForTag(TagString);
+  console.log(FoundTag);
 
-    //Refresh page
-    return res.redirect('/tagTest');
+  //Refresh page
+  return res.redirect("/tagTest");
 });
 
-router.route('/testing').get(async (req, res) => {
-    return res.render('void', {layout: "default", title: "void-page"})
-})
+router.route("/testing").get(async (req, res) => {
+  return res.render("void", { title: "void-page" });
+});
 
 export default router;
