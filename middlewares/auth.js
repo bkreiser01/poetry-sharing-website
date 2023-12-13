b50687faea10dfae501f0386e0f8b8d13522adb7
Brandon Kreiser <bkreiser5@gmail.com>
2023-12-13 14:04:42 -0500

    various merge fixes

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,17 +4,13 @@
  */
 const exports = {
     /**
-     * Middleware to force the user to login or register
+     * Middleware to not allow logged in users to access login and register pages
      * 
      * @param {Object} req - Request object
      * @param {Object} res - Response object
      * @param {Function} next - Next function
      */
-    forceLogin(req, res, next) {
-        if (!req.session.user && req.originalUrl != '/login' && req.originalUrl != '/register') {
-            return res.redirect('/login');
-        }
-
+    restrictions(req, res, next) {
         if (req.session.user && (req.originalUrl == '/login' || req.originalUrl == '/register')) {
             return res.redirect('/');
         }
