d41174fff056e06c0b931576a29bac8ac0ec7c2a
Roger Shagawat <34222430+rogerShagawat@users.noreply.github.com>
2023-12-13 23:17:35 -0500

    merged tags-data

--- helpers/validation.js
+++ helpers/validation.js
@@ -3,23 +3,23 @@ import publicValidation from "../public/js/validation.js";
 import validUrl from "valid-url";
 
 const exportedMethods = {
-  // Import the validation methods from public/js/validation.js
-  ...publicValidation,
+   // Import the validation methods from public/js/validation.js
+   ...publicValidation,
 
-  checkId(id, varName) {
-    // TODO: Check if object is in a collection
-    if (!id) throw new Error(`Error: You must provide a ${varName}`);
-    if (typeof id !== "string")
-      throw new Error(`Error: ${varName} must be a string`);
-    id = id.trim();
-    if (id.length === 0)
-      throw new Error(
-        `Error: ${varName} cannot be an empty string or just spaces`
-      );
-    if (!ObjectId.isValid(id))
-      throw new Error(`Error: ${varName} invalid ObjectId`);
-    return id;
-  },
+   checkId(id, varName) {
+      // TODO: Check if object is in a collection
+      if (!id) throw new Error(`Error: You must provide a ${varName}`);
+      if (typeof id !== "string")
+         throw new Error(`Error: ${varName} must be a string`);
+      id = id.trim();
+      if (id.length === 0)
+         throw new Error(
+            `Error: ${varName} cannot be an empty string or just spaces`
+         );
+      if (!ObjectId.isValid(id))
+         throw new Error(`Error: ${varName} invalid ObjectId`);
+      return id;
+   },
 
    checkStringArray(arr, varName) {
       //We will allow an empty array for this,
@@ -28,9 +28,11 @@ const exportedMethods = {
          throw new Error(`You must provide an array of ${varName}`);
       }
       for (let i in arr) {
-      if (typeof arr[i] !== "string" || arr[i].trim().length === 0) {
-         throw new Error(`One or more elements in ${varName} array is not a string or is an empty string` );
-      }
+         if (typeof arr[i] !== "string" || arr[i].trim().length === 0) {
+            throw new Error(
+               `One or more elements in ${varName} array is not a string or is an empty string`
+            );
+         }
       }
       return bool;
    },
@@ -62,27 +64,29 @@ const exportedMethods = {
       return url;
    },
 
-  /**
-   * Validate the taggedPoem object
-   * 
-   * @param {Object} tp 
-   */
-  checkTaggedPoem(obj) {
-    // tagged poems must have 2 keys
-    if (Object.keys(obj).length != 2) {
-      throw new Error('A taggedPoem object must have 2 keys')
-    }
+   /**
+    * Validate the taggedPoem object
+    *
+    * @param {Object} tp
+    */
+   checkTaggedPoem(obj) {
+      // tagged poems must have 2 keys
+      if (Object.keys(obj).length != 2) {
+         throw new Error("A taggedPoem object must have 2 keys");
+      }
 
-    // tagged poems must have the following keys
-    //    poemId, tagIds
-    let missingKeys = this.checkObjKeys(obj, ["poemId", "tagIds"])
-    if (missingKeys.length != 0) {
-      throw new Error(`The taggedPoem object is missing the key(s) ${missingKeys}`)
-    }
+      // tagged poems must have the following keys
+      //    poemId, tagIds
+      let missingKeys = this.checkObjKeys(obj, ["poemId", "tagIds"]);
+      if (missingKeys.length != 0) {
+         throw new Error(
+            `The taggedPoem object is missing the key(s) ${missingKeys}`
+         );
+      }
 
-    // TODO: the object ID's inside the tagged poems should all be validated
-    return obj
-  },
+      // TODO: the object ID's inside the tagged poems should all be validated
+      return obj;
+   },
 
    checkId(id, varName) {
       if (!id) throw new Error(`Error: You must provide a ${varName}`);
@@ -122,6 +126,21 @@ const exportedMethods = {
          throw new Error(
             `Error: ${strVal} is not a valid value for ${varName} as it only contains digits`
          );
+      return strVal;
+   },
+
+   /**
+    * Checks if a tagString is valid (no spaces), also makes it lowercase
+    *
+    * @param {string} strVal
+    * @param {string} varName
+    */
+   checkTagString(strVal, varName) {
+      strVal = exportedMethods.checkString(strVal, varName);
+      strVal = strVal.toLowerCase();
+      if (/\s/g.test(strVal)) {
+         throw new Error(`Error: ${varName} must not have white space`);
+      }
       return strVal;
    },
 
