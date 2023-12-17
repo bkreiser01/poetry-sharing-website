af30bcd3a0e88e57899ee09454086b3bceab123f
Roger Shagawat <34222430+rogerShagawat@users.noreply.github.com>
2023-12-17 16:54:48 -0500

    merged rshagawat

--- public/js/poems/new-poem.js
+++ public/js/poems/new-poem.js
@@ -5,21 +5,21 @@ import validation from "/public/js/validation.js";
       title_error = $("#title_error"),
       body_error = $("#body_error"),
       link_error = $("#link_error"),
-      private_error = $("#private_error"),
+      // private_error = $("#private_error"),
       success = $("#success");
 
    form.submit(function (event) {
       let poemId = $("#poemId").text(),
          title = $("#title_input").val().trim(),
          body = $("#poem-body-area").val().trim(),
-         linkInput = $("#reading-link").val().trim(),
-         priv = $("#private-checkbox").val();
+         linkInput = $("#reading-link").val().trim();
+         // priv = $("#private-checkbox").val();
 
       let errors_exist = false;
       title_error.text("");
       body_error.text("");
       link_error.text("");
-      private_error.text("");
+      // private_error.text("");
 
       let data = {};
       try {
@@ -51,9 +51,9 @@ import validation from "/public/js/validation.js";
          }
       }
 
-      if (["true", "false"].includes(priv)) {
-         data.priv = priv;
-      }
+      // if (["true", "false"].includes(priv)) {
+      //    data.priv = priv;
+      // }
 
       if (!errors_exist) return true;
       return false;
