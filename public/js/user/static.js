8f6d65aec353385fc548d33036173b1e58cc8592
jvasallo111 <jvasallo@stevens.edu>
2023-12-17 15:59:07 -0500

    Fixed user page
    Changed "Favorite tags" to display favorite tags of viewed user instead of viewing user

--- public/js/user/static.js
+++ public/js/user/static.js
@@ -3,7 +3,8 @@
         globalTagsList = $('#global-fav-tags'),
         follower_count = $('#follower_count')[0],
         following_count = $('#following_count')[0],
-        userId = $('#userId')[0].innerText
+        userId = $('#userId')[0].innerText,
+        ViewedUserId = $('#userViewId')[0].innerText;
 
     
 
@@ -43,7 +44,7 @@
     });
 
     $.ajax({
-        url: `/user/${userId}/favorite-tags`,
+        url: `/user/${ViewedUserId}/favorite-tags`,
         type: 'GET',
         success: function (tags) {
             for (let i=0; i<tags.length; i++) { 
