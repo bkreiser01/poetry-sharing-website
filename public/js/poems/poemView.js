d44e7bc43280814102028ad727e2d6a204fe9545
Brandon Kreiser <bkreiser5@gmail.com>
2023-12-17 11:48:56 -0500

    finsihed up tag functionality

--- public/js/poems/poemView.js
+++ public/js/poems/poemView.js
@@ -7,6 +7,10 @@ import validation from '/public/js/validation.js';
         userId = $('#userId')[0].innerText,
         poemId_ = $('#poemId_')[0].innerText,
         tagString = $('#TagString'),
+        showTagsBtn = $('#showTagsBtn'),
+        globalTags = $('#global-tags'),
+        userTags = $('#user-tags'),
+        tag_view = $('#tag-view'),
         error = $('#error-tag')
 
     $.ajax({
@@ -42,8 +46,6 @@ import validation from '/public/js/validation.js';
         });
     });
 
-
-
     likeBtn.on('click', function (event) {
         event.preventDefault();
         let method = 'DELETE';
@@ -67,5 +69,77 @@ import validation from '/public/js/validation.js';
             },
         });
     });
+
+    showTagsBtn.on('click', function (event) {
+        event.preventDefault();
+        if (showTagsBtn.text() == "Show Tags") {
+            tag_view.show()
+
+            // Get global tags
+            $.ajax({
+                url: `/poems/getPoemById/${poemId_}`,
+                type: 'GET',
+                success: function (poem) {
+                    for (let i = 0; i < poem.submittedTags.length; i++) {
+                        let tagId = poem.submittedTags[i].tagId;
+                        $.ajax({
+                            url: `/tags/info/${tagId}`,
+                            type: 'GET',
+                            success: function (tag) {
+                                globalTags.append(`<li><a href="/tags/${tag._id}">${tag.tagString}</a></li>`)
+                            },
+                            error: function (e) {
+                                console.error(e);
+                            }
+                        });
+                    }
+                },
+                error: function (e) {
+                    console.error(e);
+                }
+            });
+
+            // Get user tags
+            $.ajax({
+                url: `/user/getTaggedPoems/${userId}`,
+                type: 'GET',
+                success: function (taggedPoems) {
+                    let poemTagged
+                    for (let i = 0; i < taggedPoems.length; i++) {
+                        if (taggedPoems[i].poemId == poemId_) {
+                            poemTagged = taggedPoems[i]
+                        }
+                    }
+                    if (!poemTagged) {
+                        userTags.append(`<li>No tags yet. Add one!</li>`)
+                    } else {
+                        console.log(poemTagged)
+                        for (let i = 0; i < poemTagged.tagIds.length; i++) {
+                            $.ajax({
+                                url: `/tags/info/${poemTagged.tagIds[i]}`,
+                                type: 'GET',
+                                success: function (tag) {
+                                    userTags.append(`<li><a href="/tags/${tag._id}">${tag.tagString}</a></li>`)
+                                },
+                                error: function (e) {
+                                    console.error(e);
+                                }
+                            });
+                        }
+                    }
+                },
+                error: function (e) {
+                    console.error(e);
+                }
+            });
+
+            showTagsBtn.text("Hide Tags")
+        } else {
+            tag_view.hide()
+            globalTags.empty()
+            userTags.empty()
+            showTagsBtn.text("Show Tags")
+        }
+    })
 
 })(window.jQuery)
